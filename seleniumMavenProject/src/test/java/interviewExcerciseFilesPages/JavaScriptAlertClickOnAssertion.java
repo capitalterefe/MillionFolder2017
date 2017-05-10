038e0410d6339c0f49c0133b6300088eacdf1b76
wenesh <wenesh@edilawitpc>
2017-05-09 20:02:17 -0400

    finished popup handling

---
+++ seleniumMavenProject/src/test/java/interviewExcerciseFilesPages/JavaScriptAlertClickOnAssertion.java
@@ -0,0 +1,40 @@
+package interviewExcerciseFilesPages;
+
+import org.common.Utility;
+import org.common.WebDriverHelper;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+public class JavaScriptAlertClickOnAssertion extends WebDriverHelper {
+
+	Utility utility = new Utility();
+	By javascriptaletrs = By.xpath("//h3[text()='JavaScript Alerts']");
+	By jsAlert = By.xpath("//button[text()='Click for JS Alert']");
+	By jsConfirm = By.xpath("//button[text()='Click for JS Confirm']");
+	By jsPrompt = By.xpath("//button[text()='Click for JS Prompt']");
+	By jsPromptTxt = By.xpath("//button[text()='Click for JS Prompt']");
+	public void verifyJavaScriptAlertsClickOn() {
+		String jsAlertTxt = utility.getElementText(javascriptaletrs);
+		Assert.assertEquals(jsAlertTxt, "JavaScript Alerts");
+
+	}
+
+	public void ClickForJSAlert() {
+		utility.clickOn(jsAlert);
+		utility.handlePopupHitOk();
+
+	}
+
+	public void ClickForJSConfirm() {
+		utility.clickOn(jsConfirm);
+		utility.handlePopupHitCancel();
+	}
+
+	public void ClickForJSPrompt()  {
+		utility.clickOn(jsPrompt);
+	
+		utility.typeInto(jsPrompt, "ali");
+		utility.handlePopupHitOk();
+
+	}
+}
--- seleniumMavenProject/src/test/java/runner/InterViewExcerciseFiles.java
+++ seleniumMavenProject/src/test/java/runner/InterViewExcerciseFiles.java
@@ -4,10 +4,12 @@ import org.common.WebDriverHelper;
 import org.testng.annotations.Test;
 
 import interviewExcerciseFilesPages.AllLinksPage;
+import interviewExcerciseFilesPages.JavaScriptAlertClickOnAssertion;
 
 public class InterViewExcerciseFiles extends WebDriverHelper {
 
 	AllLinksPage allLinksPage = new AllLinksPage();
+	JavaScriptAlertClickOnAssertion  popUpHandling = new JavaScriptAlertClickOnAssertion();
 
 	// @Test
 	public void uploadAfile() {
@@ -19,8 +21,16 @@ public class InterViewExcerciseFiles extends WebDriverHelper {
 	}
 
 	@Test
-	public void testAlerts() {
+	public void testAlerts()  {
 		allLinksPage.clickOnJavaScriptAlerts();
+		popUpHandling.ClickForJSAlert();
+		popUpHandling.ClickForJSConfirm();
+		popUpHandling.ClickForJSPrompt();
+		
+		
+		
+	
 
 	}
+
 }
